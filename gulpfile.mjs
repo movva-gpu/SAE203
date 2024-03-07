e67194a00b1fce8c5e5102fdaf7434fae0afb843
Movva <81530181+movva-gpu@users.noreply.github.com>
2024-03-07 23:28:05 +0100

    fix(build): :rocket: added gulp
    gulp file does:
      - minify CSS
      - minify JS
      - minify PHP
      - copy everything to build/
      - deploy on my VPS
    added a conf example
    updated .gitignore
    
    Signed-off-by: Movva <81530181+movva-gpu@users.noreply.github.com>

---
+++ gulpfile.mjs
@@ -0,0 +1,95 @@
+import fs from 'node:fs';
+import uglifyEs from 'uglify-es';
+import gulp from 'gulp';
+import replace from 'gulp-replace';
+import header from 'gulp-header';
+
+import composer from 'gulp-uglify/composer.js';
+import babel from 'gulp-babel';
+import phpMinifier from '@cedx/php-minifier';
+
+import postcss from 'gulp-postcss';
+import postcssNested from 'postcss-nested';
+
+import autoprefixer from 'autoprefixer';
+import cssnano from 'cssnano';
+
+import * as child_process from "node:child_process";
+import * as config from './conf/conf.json' assert {type: 'json'};
+
+const {series, parallel, src, dest, task} = gulp;
+
+const exec = child_process.exec;
+
+const conf = config.default;
+
+const uglify = composer(uglifyEs, console)
+
+task('cleanBuild', (cb) => {
+    try {
+        fs.accessSync(import.meta.dirname + '/build')
+        fs.rmSync(import.meta.dirname + '/build/', {recursive: true, force: true});
+        cb()
+    } catch (e) {
+        cb(e)
+    }
+});
+
+task('minifyCSS', () => {
+    const plugins = [
+        autoprefixer(),
+        postcssNested(),
+        cssnano()
+    ]
+    return src('assets/css/*.css', '!*.min.css')
+        .pipe(postcss(plugins))
+        .pipe(header('/* minified */\n'))
+        .pipe(dest('build/assets/css'));
+});
+
+task('minifyPHP', () => {
+    return src(['**/*.php', '**/*.inc', '**/*.module', '!node_modules/**/*', '!vendor/**/*'])
+        .pipe(phpMinifier({mode: 'safe', binary: '/mnt/c/php-8.3.2/php.exe', silent: false}))
+        .pipe(replace('    ', ''))
+        .pipe(replace('   ', ''))
+        .pipe(replace('  ', ''))
+        .pipe(replace(/\r|\n/g, ''))
+        .pipe(dest('build/'));
+});
+
+task('minifyJS', () => {
+    return src(['assets/js/*.js', '!*.min.js', '!node_modules/**/*'])
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(uglify({
+            toplevel: true,
+            compress: {
+                global_defs: {
+                    "@console.log": "alert"
+                },
+                passes: 2
+            },
+            output: {
+                beautify: false,
+                preamble: "/* uglified */"
+            }
+        }))
+        .pipe(dest('build/assets/js'));
+});
+
+task('copyImages', () => {
+    return src('assets/img/**/*')
+        .pipe(dest('build/assets/img/'));
+});
+
+task('upload', (cb) => {
+    const command = 'sshpass -p' + atob(conf.passwd) + ' scp -r build/ ' + conf.dest;
+    const child = exec(command, (error, stdout, stderr) => {
+        console.log(stdout)
+        console.log(stderr)
+        cb(error);
+    });
+});
+
+task('default', series('cleanBuild', parallel('minifyCSS', 'minifyJS', 'minifyPHP', 'copyImages'), 'upload'));
