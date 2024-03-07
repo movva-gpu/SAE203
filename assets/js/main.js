39381d2ac9d880784aad5adc7101ed94dc794224
Movva <81530181+movva-gpu@users.noreply.github.com>
2024-03-07 10:23:15 +0100

    fix(front): :bug: .back didn't work in listing
    Signed-off-by: Movva <81530181+movva-gpu@users.noreply.github.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 window.onload = () => {
-    document.querySelector('button.btn.back').onclick = e => {
+    document.querySelector('button.back').onclick = e => {
         e.preventDefault();
         open('/', '_self')
     }
